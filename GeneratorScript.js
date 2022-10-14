f4556cef066297d3b24cb27cfebb84c9cea05661
rudynakodach <107307636+rudynakodach@users.noreply.github.com>
2022-10-14 10:46:49 +0200

    Add files via upload

--- GeneratorScript.js
+++ GeneratorScript.js
@@ -6,10 +6,10 @@ function RandomNumberGenerator(max) {
 function GenerateLink()
 {
     var currentLink = ""
-    let characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    let characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
     for (let index = 0; index < 6; index++) {
-        currentLink += characters[RandomNumberGenerator(characters.length)]
+        currentLink += characters[RandomNumberGenerator(characters.length - 2)]
     }
     completeLink = "https://prnt.sc/" + currentLink
     
